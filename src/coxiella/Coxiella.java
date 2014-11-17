5d8c95389b9689134dd7f011996628bd7a046080
harrer <tobiasharrer@gmail.com>
2014-11-17 16:10:36 +0100

    read protein sequence and function

--- src/coxiella/Coxiella.java
+++ src/coxiella/Coxiella.java
@@ -1,7 +1,6 @@
 package coxiella;
 
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
@@ -15,30 +14,36 @@ public class Coxiella {
     public void extractFasta(String file) throws IOException{
         BufferedReader br = new BufferedReader(new FileReader(file));
         StringBuilder sb = new StringBuilder();
-        String line, key, sequence;
-        HashMap<String, String> map = new HashMap<>();
-        key = br.readLine().split("@")[1];
+        String line, key, function;
+        HashMap<String, String> seq_map = new HashMap<>(), fun_map = new HashMap<>();
+        line = br.readLine();
+        key = line.split("@")[1];
+        function = line.split("   ")[1];
+        fun_map.put(key, function);
         while((line = br.readLine()) != null){
             if(line.startsWith(">")){
-                map.put(key, sb.toString());
+                seq_map.put(key, sb.toString());
                 sb.delete(0, sb.length());
                 key = line.split("@")[1];
+                function = line.split("   ")[1];
+                fun_map.put(key, function);
             }
             else{
                 sb.append(line);
             }
         }
+        seq_map.put(key, sb.toString());
         br.close();
     }
 
     /**
      * @param args the command line arguments
+     * @throws java.io.IOException
      */
     public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Coxiella cox = new Coxiella();
-        cox.extractFasta("/Users/Tobias/Downloads/p3_r57631_Cox_burne.fa");
-        System.out.println("");
+        cox.extractFasta("/Users/Tobias/Desktop/p3_r57631_Cox_burne.fa");
     }
     
 }

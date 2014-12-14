27ce4d52578c458613e97f8ee71ad6edd1abe032
tobias <tobias@tobias-K52F>
2014-12-14 22:30:30 +0100

    modfied locus tag mapper added figfam/locus tag differentiation

--- src/patric/Mapper.java
+++ src/patric/Mapper.java
@@ -6,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 
 /**
@@ -16,6 +17,7 @@ public class Mapper {
 
     private final HashMap<String, String> locus_group = new HashMap();
     private final HashMap<String, boolean[]> group_genome = new HashMap();
+    private final HashMap<String, Integer> group_families = new HashMap();
 
     private void processFile(String file) throws IOException {
         BufferedReader br = new BufferedReader(new FileReader(file));
@@ -24,6 +26,7 @@ public class Mapper {
         while ((line = br.readLine()) != null && !line.equals("")) {
             String[] split = line.split("\t");//0: Group, 1:Genome , 3:Locus
             locus_group.put(split[3], split[0]);
+            group_families.put(split[0], group_families.containsKey(split[0])? group_families.get(split[0])+1 : 1);
             boolean[] genomes = (group_genome.containsKey(split[0])) ? group_genome.get(split[0]) : new boolean[3];//bit vector genomes: 0: Q177, 1: Q154, 2: RSA 493
             if (split[1].equalsIgnoreCase("Coxiella burnetii 'MSU Goat Q177'")) {
                 genomes[0] = true;
@@ -38,12 +41,16 @@ public class Mapper {
     }
     
     private void toFile(String path) throws FileNotFoundException{
-        StringBuilder sb = new StringBuilder("Patric Locus Tag\tQ177\tQ154\tRSA_493\n");
+        StringBuilder sb = new StringBuilder("Locus Tag/FigFam\tQ177\tQ154\tRSA_493\n");
+        HashSet<String> groups = new HashSet();
         for (Map.Entry<String, String> entrySet : locus_group.entrySet()) {
             String locus = entrySet.getKey();
             String group = entrySet.getValue();
             boolean[] genomes = group_genome.get(group);
-            sb.append(locus).append('\t').append(genomes[0]).append('\t').append(genomes[1]).append('\t').append(genomes[2]).append('\n');
+            if(!groups.contains(group)){
+                sb.append(group_families.get(group) == 1? locus : group).append('\t').append(genomes[0]).append('\t').append(genomes[1]).append('\t').append(genomes[2]).append('\n');
+            }
+            groups.add(group);
         }
         PrintWriter pw = new PrintWriter(path);
         pw.write(sb.toString());
@@ -53,7 +60,6 @@ public class Mapper {
     public static void main(String[] args) throws IOException {
         Mapper m = new Mapper();
         m.processFile("/home/tobias/Desktop/ProteinFamilyFeatures.txt");
-        m.toFile("/home/tobias/Desktop/out.txt");
-        System.out.println("");
+        m.toFile("/home/tobias/Desktop/genomes_figfam_locus.txt");
     }
 }

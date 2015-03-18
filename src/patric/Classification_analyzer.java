f797381c1b78aff5df78993649d179918f6f0325
harrer <tobiasharrer@gmail.com>
2015-03-18 21:03:56 +0100

    automatic clustering of MSAs

--- src/patric/Classification_analyzer.java
+++ src/patric/Classification_analyzer.java
@@ -6,11 +6,9 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
-import java.util.function.Consumer;
 import translation.Diff;
 import lcs.LCS;
 
